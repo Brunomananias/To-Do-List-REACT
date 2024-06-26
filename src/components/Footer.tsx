27a02640d0f8fae0832502f4ffac3123ba1e8d7b
Bruno Ananias <brunomananias2009@hotmail.com>
2024-06-25 22:06:02 -0300

    first commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Styles from './Footer.module.css'
+
+const Footer = () => {
+    return (
+        <footer className={Styles.footer}>
+        <p>
+          <span>React + TS Todo 2024</span>
+        </p>
+      </footer>
+    );
+};
+
+export default Footer;
+
