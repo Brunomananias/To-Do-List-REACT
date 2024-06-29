c7ad9e7d4b0553e55c498a48eeadff1e4338952b
Bruno Ananias <brunomananias2009@hotmail.com>
2024-06-29 19:39:56 -0300

    Finalizei a taskform e tasklist

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,15 +1,32 @@
 import React from 'react'
+import { ITask } from '../interfaces/Task'
+import styles from './TaskList.module.css'
 
 interface Props {
-
+ tasklist: ITask[];
 }
 
-const TaskList = (props: Props) => {
+const TaskList = ({tasklist}: Props) => {
     return (
-        <div>
-            Lista de tarefas
-        </div>
-    )
-}
+        <>
+        {tasklist.length > 0 ? (
+            tasklist.map((task) => (
+                <div key={task.id}>
+                    <div>
+                        <h4>{task.title}</h4>
+                        <p>Dificuldade: {task.difficulty}</p>
+                    </div>
+                    <div>
+                        <i className='bi bi-pencil'></i>
+                        <i className='bi bi-trash'></i>
+                    </div>
+                </div>
+            ))
+        ) : (
+            <p>Não tem tarefas cadastradas</p>
+        )}
+        </>
+    );
+};
 
 export default TaskList;
